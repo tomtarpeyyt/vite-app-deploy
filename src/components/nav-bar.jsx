b8c296275c6d44de5dc91590bac58743e820252a
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-24 22:12:50 +0100

    fixed light mode

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -1,5 +1,5 @@
 import { Menu, X, MoonStar, SunMoon } from 'lucide-react';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.svg';
 import { navItems } from '../constants';
 import { useEffect, useState } from 'react';
 
@@ -15,17 +15,17 @@ const Navbar = () => {
         const body = document.body;
     
         if (isDarkMode) {
-          body.classList.add('dark-mode');
-          body.classList.remove('light-mode');
+            body.classList.add('dark-mode');
+            body.classList.remove('light-mode');
         } else {
-          body.classList.add('light-mode');
-          body.classList.remove('dark-mode');
+            body.classList.add('light-mode');
+            body.classList.remove('dark-mode');   
         }
     
         return () => {
-          body.classList.remove('dark-mode');
-          body.classList.remove('light-mode');
-        };
+            body.classList.remove('dark-mode');
+            body.classList.remove('light-mode');
+        }
       }, [isDarkMode]);
     
     const toggleNavOpen = () => {
@@ -37,7 +37,7 @@ const Navbar = () => {
                 <div className="container px-4 mx-auto relative lg:text-sm">
                     <div className="flex justify-between items-center">
                         <div className="flex items-center flex-shrink-0">
-                            <img src={ logo } alt="logo" className="h-10 w-10 mr-2" />
+                            <img src={ logo } alt="logo" className="h-8 w-8 mr-2" />
                             <span className="text-xl tracking-tight">ComputR</span>
                         </div>
                         <ul className="hidden lg:flex ml-14 space-x-12">
@@ -54,14 +54,16 @@ const Navbar = () => {
                             <a href="#" className="border px-3 py-2 rounded-md">Sign In</a>
                             <a href="#" className="bg-gradient-to-r from-blue-500 to-blue-800 px-3 py-2 rounded-md">Create an Account</a>
                         </div>
-                        <div className="lg:hidden md:flex flex-col justify-end">
+                        <div className="lg:hidden md:flex justify-end">
+                        <button className="mr-4" onClick={ toggleDarkMode }>{ isDarkMode ? <MoonStar /> : <SunMoon /> }</button>
                             <button onClick={ toggleNavOpen }>
                                 { navOpen ? <X /> : <Menu /> }
                             </button>
                         </div>
                     </div>
                     {navOpen && (
-                        <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                        <div className="fixed right-0 z-20 bg-neutral-30 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+
                             <ul className='mb-6'>
                             {navItems.map((item, index) => (
                                 <li key={index} className="py-4">
@@ -70,7 +72,6 @@ const Navbar = () => {
                             ))}
                             </ul>
                             <div className="flex space-x-6">
-                                <button onClick={ toggleDarkMode }>{ isDarkMode ? <MoonStar /> : <SunMoon /> }</button>
                                 <a href="#" className="px-3 py-2 border rounded-md">Sign In</a>
                                 <a href="#" className="px-3 py-2 rounded-md bg-gradient-to-r from-blue-500 to-blue-800">Create an account</a>
                             </div>
@@ -78,9 +79,6 @@ const Navbar = () => {
                     )}
                 </div>
             </nav>
-
-
-
 
         </>
     );
